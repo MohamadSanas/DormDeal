069250317a6eded2328a324f941ba03521e019f1
Mohroof Mohamad sanas <159630755+MohamadSanas@users.noreply.github.com>
2026-09-22 21:37:22 +0530

    chore(build): configure AndroidManifest permissions and networking for release

--- DormDeal/lib/services/api_service.dart
+++ DormDeal/lib/services/api_service.dart
@@ -38,7 +38,7 @@ class ApiService {
     }
     try {
       if (Platform.isAndroid) {
-        return 'http://10.0.2.2:8000/api/v1';
+        return 'http://127.0.0.1:8000/api/v1';
       }
     } catch (_) {}
     return 'http://127.0.0.1:8000/api/v1';
